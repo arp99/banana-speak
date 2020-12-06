ee2da3b92233ecb44c03cc83ddf522374c743504
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2020-12-06 20:07:07 +0530

    Add api call functionality

---
+++ index.js
@@ -0,0 +1,31 @@
+const inputText = document.querySelector('#input-text')
+const outputText = document.querySelector('#output-text')
+const btnTranslate = document.querySelector('button')
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTraslationURL(text){
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(err){
+    console.log(err);
+    alert("Cannot translate right now 😞");
+}
+
+function clickHandler(){
+    var textInput = inputText.value
+    var url = getTraslationURL(textInput);
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(json => showTranslation(json))
+    .catch(errorHandler)
+}
+
+function showTranslation(json){
+    var translatedText = json.contents.translated;
+    outputText.value = translatedText;
+}
+
+btnTranslate.addEventListener('click', clickHandler)
